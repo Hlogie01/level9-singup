65bc116492b246cf34ca0c3158db8ba21e5c9b16
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-08-23 13:03:04 +0200

    logo update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import "./index.css";
-import App from "./App";
-import { store } from "./store/reduxStore";
+import logo from "../assets/Logo.png";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-        <App />
-      </GoogleOAuthProvider>
-    </Provider>
-  </React.StrictMode>
-);
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default Logo;
